3c72e32e176c40f3bb4f5d736fade76949a057b0
Neo Maxwell Tartan <neomaxwell27@gmail.com>
2026-03-10 20:46:34 +0700

    Removed Fast Script Reload
    made game broke

--- Assets/Scripts/Fishing/PondItem.cs
+++ Assets/Scripts/Fishing/PondItem.cs
@@ -2,7 +2,14 @@ using UnityEngine;
 
 public class PondItem: MonoBehaviour {
     public float speed = 5f;
-
+    public float speedDeviation = 1f;
+    public float sizeDeviation = 0.1f;
+    private void Start() {
+        speed = UnityEngine.Random.Range(speed - speedDeviation, speed + speedDeviation);
+        float sizeMultiplier = UnityEngine.Random.Range(1f-sizeDeviation, 1f+sizeDeviation);
+        transform.localScale = new Vector3(sizeMultiplier, sizeMultiplier, sizeMultiplier);
+        Debug.Log(transform.scale);
+    }
     void Update() {
         transform.Translate(Vector3.right * speed * Time.deltaTime);
         
