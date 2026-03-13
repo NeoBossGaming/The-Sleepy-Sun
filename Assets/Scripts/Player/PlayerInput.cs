6826c110df27011fbbd9bff4c3f73c5e1a209397
Neo Maxwell Tartan <neomaxwell27@gmail.com>
2026-03-13 18:14:33 +0700

    Code OPtimization
    Polished input system

---
+++ Assets/Scripts/Player/PlayerInput.cs
@@ -0,0 +1,73 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerInput : MonoBehaviour
+{
+    // Action Reference
+    InputAction moveInput;
+    InputAction lookInput;
+    InputAction dashInput;
+    InputAction interactInput;
+    InputAction previousInput;
+    InputAction nextInput;
+    InputAction sprintInput;
+
+    // Action Value
+    public struct inputValues
+    {
+        public Vector2 move;
+        public Vector2 look;
+        public bool dash;
+        public bool interact;
+        public bool previous;
+        public bool next;
+        public  bool sprint;
+    }
+
+    private inputValues currentInputValues;
+
+    void Start()
+    {
+        // Initialize All Actions
+        moveInput = InputSystem.actions.FindAction("Move");
+        lookInput = InputSystem.actions.FindAction("Look");
+        dashInput = InputSystem.actions.FindAction("Dash");
+        interactInput = InputSystem.actions.FindAction("Interact");
+        previousInput = InputSystem.actions.FindAction("Previous");
+        nextInput = InputSystem.actions.FindAction("Next");
+        sprintInput = InputSystem.actions.FindAction("Sprint");
+
+        moveInput.Enable();
+        lookInput.Enable();
+        dashInput.Enable();
+        interactInput.Enable();
+        previousInput.Enable();
+        nextInput.Enable();
+        sprintInput.Enable();
+    }
+
+    void Update()
+    {
+        // Obtain Vector 2 Input Values
+        currentInputValues.move = moveInput.ReadValue<Vector2>();
+        currentInputValues.look = lookInput.ReadValue<Vector2>();
+
+        // Obtain Bool Buttons Input Values
+        currentInputValues.dash = dashInput.WasPressedThisFrame();
+        currentInputValues.interact = interactInput.WasPressedThisFrame();
+        currentInputValues.previous = previousInput.WasPressedThisFrame();
+        currentInputValues.next = nextInput.WasPressedThisFrame();
+
+        // Obtain Continous Button Input Values
+        currentInputValues.sprint = sprintInput.IsPressed();
+    }
+
+    public inputValues obtainMoveInputActions
+    {
+        get
+        {
+            return currentInputValues;
+        }
+    } 
+
+}
